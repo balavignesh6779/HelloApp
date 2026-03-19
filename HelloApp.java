5de3fd4d57f736e10b73b1ea22e9ddced5a38de0
balavignesh6779 <balavigneshp06@gmail.com>
2026-03-19 13:23:39 +0530

    uc2

---
+++ HelloApp.java
@@ -0,0 +1,9 @@
+
+public class HelloApp {
+    public static void main(String[] args) {
+        // Step 1: Read the name from the args[0] parameter
+        String name = args[0];
+        // Step 2: Display personalized greeting to the console
+        System.out.println("Hello, " + name + "!");
+    }
+}
