c4be2be46b2f5725022853770fb4d88f69b2e3a9
balavignesh6779 <balavigneshp06@gmail.com>
2026-03-26 09:21:26 +0530

    message

---
+++ ArrayStats.java
@@ -0,0 +1,34 @@
+public class ArrayStats {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter array size (min 2): ");
+        int size = input.nextInt();
+
+        if (size < 2) {
+            System.err.println("Size must be at least 2.");
+            return;
+        }
+
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + i + ": ");
+            arr[i] = input.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println("Largest: " + largest);
+        System.out.println("Second Largest: " + secondLargest);
+        input.close();
+    }
+}
